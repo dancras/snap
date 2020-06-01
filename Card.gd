0f7662a4cf7dbad6785d752e4478e736a8064b2f
Daniel Howlett <dan@dancras.co.uk>
2020-05-31 18:12:23 -0600

    Add all the different cards

--- Card.gd
+++ Card.gd
@@ -16,6 +16,13 @@ enum Rank {
     R_ACE,
 }
 
+enum Suit {
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES
+}
+
 const RANK_TEXT = {
     Rank.R_2: "2",
     Rank.R_3: "3",
@@ -32,7 +39,82 @@ const RANK_TEXT = {
     Rank.R_ACE: "A",
 }
 
+const RANK_CONFIG = {
+    Rank.R_2: {
+        "text": "2",
+        "symbol_format": "x\nx"
+    },
+    Rank.R_3: {
+        "text": "3",
+        "symbol_format": "x\nxx"
+    },
+    Rank.R_4: {
+        "text": "4",
+        "symbol_format": "xx\nxx"
+    },
+    Rank.R_5: {
+        "text": "5",
+        "symbol_format": "x\nxx\nxx"
+    },
+    Rank.R_6: {
+        "text": "6",
+        "symbol_format": "x\nxx\nxxx"
+    },
+    Rank.R_7: {
+        "text": "7",
+        "symbol_format": "x\nxx\nxx\nxx"
+    },
+    Rank.R_8: {
+        "text": "8",
+        "symbol_format": "xx\nxx\nxx\nxx"
+    },
+    Rank.R_9: {
+        "text": "9",
+        "symbol_format": "xxx\nxxx\nxxx"
+    },
+    Rank.R_10: {
+        "text": "10",
+        "symbol_format": "x\nxx\nxxx\nxxxx"
+    },
+    Rank.R_JACK: {
+        "text": "J",
+        "symbol_format": "x"
+    },
+    Rank.R_QUEEN: {
+        "text": "Q",
+        "symbol_format": "x"
+    },
+    Rank.R_KING: {
+        "text": "K",
+        "symbol_format": "x"
+    },
+    Rank.R_ACE: {
+        "text": "A",
+        "symbol_format": "x"
+    },
+}
+
+const SUIT_CONFIG = {
+    Suit.HEARTS: {
+        "symbol": "♥",
+        "color": Color.red
+    },
+    Suit.DIAMONDS: {
+        "symbol": "♦",
+        "color": Color.red
+    },
+    Suit.CLUBS: {
+        "symbol": "♣",
+        "color": Color.black
+    },
+    Suit.SPADES: {
+        "symbol": "♠",
+        "color": Color.black
+    }
+}
+
 export(Rank) var rank: int
+export(Suit) var suit: int
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -40,8 +122,24 @@ export(Rank) var rank: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $Rank.text = RANK_TEXT[rank]
+    var rank_config = RANK_CONFIG[rank]
+    var active_suit = $Suit
 
+    if (rank == Rank.R_10):
+        $Rank.margin_left -= 5
+
+    if (rank >= Rank.R_JACK):
+        active_suit = $SuitLarge
+
+    $Suit.hide()
+    $SuitLarge.hide()
+    active_suit.show()
+
+    $Rank.text = rank_config.text
+    active_suit.text = rank_config.symbol_format.replace("x", SUIT_CONFIG[suit].symbol)
+
+    $Rank.add_color_override("font_color", SUIT_CONFIG[suit].color)
+    active_suit.add_color_override("font_color", SUIT_CONFIG[suit].color)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
